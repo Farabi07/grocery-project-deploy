e5a9f409978bad78813d9392b21e122b93852c33
farabi07 <farhadkabir1212@gmail.com>
2025-07-23 21:54:30 +0600

    add aws rds

--- start_project/settings.py
+++ start_project/settings.py
@@ -129,26 +129,26 @@ AUTH_USER_MODEL = 'authentication.User'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 # Use environment variables for DB config
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('AWS_NAME'),
-#         'USER': os.getenv('AWS_USER'),
-#         'PASSWORD': os.getenv('AWS_PASSWORD'),
-#         'HOST': os.getenv('AWS_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
+#         'NAME': os.getenv('POSTGRES_DB'),
+#         'USER': os.getenv('POSTGRES_USER'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+#         'HOST': os.getenv('POSTGRES_HOST'),
+#         'PORT': os.getenv('POSTGRES_PORT'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('AWS_NAME'),
+        'USER': os.getenv('AWS_USER'),
+        'PASSWORD': os.getenv('AWS_PASSWORD'),
+        'HOST': os.getenv('AWS_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -209,8 +209,8 @@ CORS_ALLOWED_ORIGINS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # media files upload directory
-# MEDIA_URL = '/media/'  # media files retrieve directory
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # media files upload directory
+MEDIA_URL = '/media/'  # media files retrieve directory
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
